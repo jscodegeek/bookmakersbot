73a59708fca83477cb80c53b2bf456156f1c2211
Eugene Safronov <evgeniy.safronov@outlook.com>
2017-11-03 23:52:48 +0200

    WH: create scrapper

--- scrappers/william_hill.js
+++ scrappers/william_hill.js
@@ -12,22 +12,72 @@ rp(URL)
 .then((html) => {
     const dom = new JSDOM(html);
     //var rows = dom.window.document.querySelectorAll('.content')[0].querySelectorAll('tr');
-    var rows_league = dom.window.document.querySelectorAll('.content')[0].querySelectorAll('.heading');
-
+    const rows_league = dom.window.document.querySelectorAll('.content')[0].querySelectorAll('.heading');
     //remove heade Football
-
     const LEAGUES = _.map(_.values(rows_league), row => row.textContent.trim());
-    
+
     //remove heade Football
-    LEAGUES.pop();
+    LEAGUES.shift();
 
-    console.log(LEAGUES);
  
-    // if (rows[1].textContent.trim() === 'German Bundesliga') {
-    //     console.log(11);
-    // }
+    const rows = _.values(dom.window.document.querySelectorAll('.content')[0].querySelectorAll('tr'));
 
+    var score = rows[3].querySelector('b').textContent.trim();
+    var event = rows[4].textContent.trim();
+    var oddWinFirst = rows[5].querySelector('.bold').textContent.trim();
+    var oddDraw = rows[6].querySelector('.bold').textContent.trim();
+    var oddWinSecond = rows[7].querySelector('.bold').textContent.trim();
+    //console.log(rows[7].querySelector('.bold').textContent.trim());
+
+    const result = [];
+    var i = 0;
+    while (i < rows.length) {
+        var rowVal = rows[i].textContent.trim()
+        if (_.includes(LEAGUES, rowVal)) {
+            var league = rows[i].textContent.trim();
+            do {
+                var score = rows[i+2].querySelector('b').textContent.trim();
+                var event = rows[i+3].textContent.trim();
+                var home = event.split(' v ')[0];
+                var away = event.split(' v ')[1];
+                var oddHome = fractionalToDecimal(rows[i+4].querySelector('.bold').textContent.trim());
+                var oddDraw = fractionalToDecimal(rows[i+5].querySelector('.bold').textContent.trim());
+                var oddAway = fractionalToDecimal(rows[i+6].querySelector('.bold').textContent.trim());
+                var match = {league, score, away, home, oddHome, oddDraw, oddAway};
+                result.push(match);
+                
+                if ((i+9)<rows.length) {
+                    var isLastMatchInLeague = _.includes(LEAGUES, rows[i+9].textContent.trim());
+                } else {
+                    var isLastMatchInLeague = true;
+                }
+
+                if (isLastMatchInLeague) {
+                    i = i + 9;
+                } else {
+                    i = i + 8;
+                }
+            } while(!isLastMatchInLeague);
+        } else {
+            i++;
+        }
+
+        
+    }
+    console.log(result);
+    console.log(result.length);
 })
 .catch((err) => {
     console.log(err);
 });
+
+const fractionalToDecimal = function (value) {
+    var fract_arr = value.split('/');
+    var dec = _.parseInt((((fract_arr[0]) / (fract_arr[1])) + 1)*100, 10)/100;
+    return dec
+}
+
+// module.exports = {
+//     sayHelloInEnglish: function() {
+//       return "HELLO";
+//     },
