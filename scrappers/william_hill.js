3a789b871d1589ee20acc3fa27dcea9e246808a7
Eugene Safronov <evgeniy.safronov@outlook.com>
2017-11-03 21:52:46 +0200

    WH scrapper:  scrap leagues football list

--- scrappers/william_hill.js
+++ scrappers/william_hill.js
@@ -1,13 +1,32 @@
 'use strict';
 
+const _ = require('lodash');
 const rp = require('request-promise');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 const URL = 'https://mobet.williamhill.com/wap/bet/en-gb/betlive/9/Football.html?refPage=inPlay';
 
 
 rp(URL)
-.then((htmlString) => {
-    console.log(htmlString);
+.then((html) => {
+    const dom = new JSDOM(html);
+    //var rows = dom.window.document.querySelectorAll('.content')[0].querySelectorAll('tr');
+    var rows_league = dom.window.document.querySelectorAll('.content')[0].querySelectorAll('.heading');
+
+    //remove heade Football
+
+    const LEAGUES = _.map(_.values(rows_league), row => row.textContent.trim());
+    
+    //remove heade Football
+    LEAGUES.pop();
+
+    console.log(LEAGUES);
+ 
+    // if (rows[1].textContent.trim() === 'German Bundesliga') {
+    //     console.log(11);
+    // }
+
 })
 .catch((err) => {
     console.log(err);
