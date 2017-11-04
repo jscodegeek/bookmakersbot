f1c1f505c9152b68a25f77a42795385f7af664f0
Eugene Safronov <evgeniy.safronov@outlook.com>
2017-11-04 03:02:28 +0200

    Paddy power: finished scrapper

--- scrappers/paddy_power.js
+++ scrappers/paddy_power.js
@@ -2,25 +2,39 @@
 
 const _ = require('lodash');
 const rp = require('request-promise');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
 
-const URL = 'http://live.paddypower.com/#/';
+const URL = 'https://sports.paddypower.mobi/tps/live';
 
 
 rp(URL)
-.then((html) => {
-    console.log(html);
+.then((res) => {
+    let data = JSON.parse(res);
+    let sports = data.sports
+    let football = _.find(sports, { 'id': 49 });
+    let competitions = football.competitions;
+    
+
+    let result = [];
+
+    _.each(competitions, comp => {
+        let league = comp.title;
+            let event = comp.events[0];
+        let away = event.awayName;
+        let home = event.homeName;
+            let outcomes = event.markets[0].outcomes
+        let oddHome = outcomes[0].oddsDecimal;
+        let oddDraw = outcomes[1].oddsDecimal;
+        let oddAway = outcomes[2].oddsDecimal;
+        let match = {league, score: null, away, home, oddHome, oddDraw, oddAway};
+        result.push(match);
+    });
+    console.log(result);
 })
 .catch((err) => {
     console.log(err);
 });
 
-const fractionalToDecimal = function (value) {
-    let fract_arr = value.split('/');
-    let dec = _.parseInt((((fract_arr[0]) / (fract_arr[1])) + 1)*100, 10)/100;
-    return dec;
-}
+
 
 // module.exports = {
 //     sayHelloInEnglish: function() {
